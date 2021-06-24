fefb871a18a18c58805ca693d2a74248d50dc7e3
Iouri Sorokine <iouri@bitwala.com>
2021-06-24 23:21:59 +0200

    parcel schema

---
+++ models/Parcel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PlantOnParcelSchema = new Schema({
+  typeOfVegetable: {
+    type: Schema.Types.ObjectId,
+    ref: "Vegetable",
+    required: true,
+  },
+  seedDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+const ParcelSchema = new Schema({
+  name: {
+    type: String,
+  },
+  size: {
+    lengthM: Number,
+    widthM: Number,
+    surfaceSQM: Number,
+  },
+  plantsOnParcel: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "PlantOnParcel",
+    },
+  ],
+});
+
+const Parcel = mongoose.model("Parcel", ParcelSchema);
+module.exports = Parcel;
